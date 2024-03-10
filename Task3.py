3951e5c769bcb852f3baa0209af2fc3df1d8be51
safarahmadov <152492893+safarahmadov@users.noreply.github.com>
2024-03-11 01:37:42 +0400

    Add files via upload

---
+++ Task3.py
@@ -0,0 +1,95 @@
+#1
+numbers=[1,2,3,4,5,6]
+# sum_list=0
+
+# for i in numbers:
+#     sum_list+=i
+# print(sum_list)
+
+###############
+
+# multip_list=1
+
+# for i in numbers:
+#     multip_list*=i
+    
+# print(multip_list)
+#####################
+#2
+# numbers.reverse()
+# print(numbers)
+#####################
+#3
+
+# name=["sa","sa",]
+# hello=["far","lam"]
+
+# for a, i in zip(name, hello):
+#     print(a,i)
+################################3
+#4
+# liste=[1,2,["apple","pear","blakcberry"]]
+# liste[2][2]="watermelon"
+# print(liste)
+# #???
+
+########################################
+#6
+# print(numbers.index(6))
+# numbers.insert(5, 55)
+# print(numbers)
+#################################################
+#7
+# num1=int(input())
+# num2=int(input())
+# for i in range(num1, num2):
+#     if i  %2==0:
+#         print("cut")
+#     else:
+#         print("tek")
+########################################
+#8
+# liste=[5,6,7,7,8,9,10,10]
+# list2=set(liste)
+# list1=[list2]
+# print(list1)
+#################################################
+#9
+# name_input=input("choose 1(add) or 2(remove)")
+# a = {'ad':['Fuad','Rovsen','Orxan']}
+# if name_input=="1":
+#     ad_input=input("add name?")
+#     a["ad"].append(ad_input)
+#     print(a)
+# else:
+#     del_input=input("which delete name?")
+#     a["ad"].remove(del_input)
+#     print(a)   
+
+#9
+# import random
+# num =[1,2,3]
+# num1=[]
+
+# for i in range(0,len(num)):
+#     if num==num:
+#         random.shuffle(num)
+#         print(num)
+    
+    
+#################################################
+#10
+# name=input("enter programming language name")
+# return_input=int(input("How many times should you turn around?"))
+# numbers=0
+
+# while numbers<=return_input:
+#     print(numbers, name)
+#     numbers+=1
+    
+
+
+
+
+
+
